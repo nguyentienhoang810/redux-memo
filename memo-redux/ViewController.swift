0692cb1804ea284cdeaf926e09826940692307d3
nguyentienhoang <nguyentienhoang@nguyentienhoangs-macbook-pro.local>
2019-02-16 11:04:54 +0900

    Initial Commit

---
+++ memo-redux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memo-redux
+//
+//  Created by nguyentienhoang on 2019/02/16.
+//  Copyright © 2019 nguyentienhoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
