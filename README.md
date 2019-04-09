5d82ba5f2e88123912b8a18b3f60fec58c8d0cae
nguyentienhoang <nguyentienhoang810@gmail.com>
2019-04-09 17:24:20 +0900

    sample add/delete action and store into tableView

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 2019/4/8
 first redux memo:
 only tap to button to add more text
+
+- Khi có 1 event được dispatch từ View, sẽ tạo ra một Action mô tả về event đó.
+- Action sẽ được gửi đến Reducer xử lý.
+- Reducer sẽ nhận Action và dưạ vào các mô tả của Action để tạo ra một State mới và lưu tại Store.
+- Các ViewController đã được subcriber đến Store sẽ nhận được sự thay đổi và tiến hành update UI.
--- memo-redux/ViewController.swift
+++ memo-redux/ViewController.swift
@@ -9,77 +9,149 @@
 import UIKit
 import ReSwift
 
-let applicationStore = Store(reducer: AppReducer, state: AppState())
+struct Task {
+    var name: String?
+}
 
 struct AppState: StateType {
-    //something like model here
-    var text: String?
+    var tasks = [Task]()
 }
 
-struct InputAction: Action { //state will be changed by action
-    var text: String
+struct AddTask: Action {
+    var name: String?
+}
+
+struct DeleteTask: Action {
+    var index: Int?
 }
 
 
-//get new state by action
-func AppReducer(action: Action, state: AppState?) -> AppState {
-    let state = state ?? AppState(text: "") //get current state
-    var newState = state //copy current state
+//Reducer sẽ dựa vào Action để trả về một State mới từ State hiện tại.
+//Như đoạn code trên nếu là action AddTask thì sẽ insert một task mới với mô tả name được truyền vào,
+//còn nếu là action DeleteTask thì sẽ delete đi task với index được mô tả trong action.
+func appReducer(action: Action, state: AppState?) -> AppState {
+    var state = state ?? AppState()
+
     switch action {
-    case _ as InputAction:
-        newState = AppState(text: (action as! InputAction).text) //process copied state
+    case let addTask as AddTask:
+        let task = Task(name: addTask.name ?? "undefined name")
+        state.tasks.insert(task, at: 0)
+    case let deleteTask as DeleteTask:
+        state.tasks.remove(at: deleteTask.index ?? 0)
     default:
-        break
+        return state
     }
-    return newState
+
+    return state
 }
+
+//Nhiệm vụ của store là lưu trữ state của ứng dụng
+//và phản hồi state mới tới các View subscribe đến nó.
+//Một ứng dụng chỉ có duy nhất một Store (nguyên lý 1 single source of truth)
+//chứa tất các state cần thiết cho ứng dụng.
+let store = Store<AppState>(reducer: appReducer, state: AppState())
+
+
 
 class ViewController: UIViewController, StoreSubscriber {
-
     func newState(state: AppState) {
-        print(state.text ?? "")
+        self.tasks = state.tasks
     }
 
     typealias StoreSubscriberStateType = AppState
+    var tasks = [Task]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
-    private let button = UIButton()
+    var taskNumber = 0
+
+    private lazy var tableView: UITableView = {
+        let tb = UITableView()
+        tb.delegate = self
+        tb.dataSource = self
+        tb.estimatedRowHeight = 200
+        return tb
+    }()
+
+    private lazy var addBtn: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .orange
+        btn.setTitle("Add task", for: .normal)
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
         addSubview()
-        button.addTarget(self, action: #selector(tap(_:)), for: .touchUpInside)
-    }
-
-    @objc func tap(_ sender: UIButton) {
-        var text = applicationStore.state.text ?? ""
-        text = text + "s"
-        applicationStore.dispatch(InputAction(text: text))
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        applicationStore.subscribe(self)
+        store.subscribe(self)
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        applicationStore.unsubscribe(self)
+        store.unsubscribe(self)
     }
 
-    private func addSubview() {
-        view.addSubview(button)
-        button.backgroundColor = .orange
-        button.setTitle("Tap Here", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            button.heightAnchor.constraint(equalToConstant: 50),
-            button.widthAnchor.constraint(equalToConstant: 200),
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-            ])
+    @objc func addTask() {
+        taskNumber += 1
+        let addTaskAction = AddTask(name: "new Task \(taskNumber)")
+        store.dispatch(addTaskAction)
     }
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let deleteAction = DeleteTask(index: indexPath.row)
+        store.dispatch(deleteAction)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = tasks[indexPath.row].name ?? "no name"
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+
+extension ViewController {
+    private func addSubview() {
+        view.addSubview(tableView)
+        view.addSubview(addBtn)
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        addBtn.translatesAutoresizingMaskIntoConstraints = false
+
+        tableView.layer.borderColor = UIColor.orange.cgColor
+        tableView.layer.borderWidth = 1
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        addBtn.addTarget(self, action: #selector(addTask), for: .touchUpInside)
+
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+
+            addBtn.heightAnchor.constraint(equalToConstant: 30),
+            addBtn.widthAnchor.constraint(equalToConstant: 200),
+            addBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
+            addBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+    }
+}
